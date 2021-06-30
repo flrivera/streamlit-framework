9e1810426614d767ba196edbe6e897b9c02fd715
flrivera <flrivera@uci.edu>
2021-06-30 16:23:12 -0700

    Add files via upload

--- Stock_app.py
+++ Stock_app.py
@@ -3,8 +3,6 @@ import json
 import streamlit as st
 import datetime as dt
 from alpha_vantage.timeseries import TimeSeries
-from bokeh.io import output_file, show
-from bokeh.plotting import figure
 import matplotlib.pyplot as plt
 import altair as alt
 import pandas as pd
@@ -13,7 +11,10 @@ import os
 from Month_dict import month_dict
 from dotenv import load_dotenv
 from urllib.error import URLError
+import SessionState
 
+
+state = SessionState.get(chat_list=[])
 
 load_dotenv() # load my enviornment variables
 
@@ -30,70 +31,85 @@ st.sidebar.header("Configuration")
 
 ticker = st.sidebar.text_input('Stock-Ticker') # make selectbox with time 
 
-#ticker=f'{ticker}'
+
 
 year_options= np.linspace(1999,2021,22)
 year_options=[int(year) for year in year_options]
-#Year=st.multiselect(year_label, year_options)
+
 Year = st.sidebar.selectbox('What year would you like data for?',options=year_options)
 
 if Year==2021:
     
-    #month_label='What month would you like data for?'
+   
     month_options=['January','February','March','April','May','June']
-#index=st.multiselect(month_label, month_options)
-#Month=month_dict[f'index']
+
     Month = st.sidebar.selectbox(f'What month would you like data for?',options=month_options)
 else:
     
 
-#month_label='What month would you like data for?'
+
     month_options=['January','February','March','April','May','June','July','August',
                'September','October','November','December']
-#index=st.multiselect(month_label, month_options)
-#Month=month_dict[f'index']
+
     Month = st.sidebar.selectbox(f'What month would you like data for?',options=month_options)
 
 Month_index=month_dict[f'{Month}']
-#ticker = input('Ticker:  ')
-#typ= input ('Data type-  "daily",  "weekly",   "monthly", "interval" :   ')
-
-ts=TimeSeries(f'{API_key}',output_format='pandas')
-
-aapl,metadata=ts.get_daily_adjusted(symbol=ticker,outputsize=outputsize)
-aapl2,metadata2=ts.get_daily(symbol=ticker,outputsize=outputsize)
-
-include=aapl[aapl.index.year==Year]
-iinclude=aapl2[aapl2.index.year==Year]
-
-include2=include[include.index.month==Month_index]
-iinclude2=iinclude[iinclude.index.month==Month_index]
-
-A=include2['4. close'].astype(float)
-B=iinclude2['4. close'].astype(float)
-
-A=pd.DataFrame(A)
-
-jo=np.array(A['4. close'])
-
-A['Date']=(A.index.day).astype(float)
-
-jo_x=np.array(A['Date'])
 
 
-B=pd.DataFrame(B)
+if st.sidebar.button("Post Stock Info"):
+    state.chat_list.append((ticker, Year, Month))
+    
+if len(state.chat_list) > 10:
+    del (state.chat_list[0])
 
-B['Date']=(B.index.day).astype(float)
+    
+try:
+    ticker,Year,Month = zip(*state.chat_list)
+    
+    ts=TimeSeries(f'{API_key}',output_format='pandas')
+    
+    try:
+
+        aapl,metadata=ts.get_daily_adjusted(symbol=ticker,outputsize=outputsize)
+        aapl2,metadata2=ts.get_daily(symbol=ticker,outputsize=outputsize)
+    except ValueError:
+        
+        st.title("Incorrect Stock Ticker") 
+    
+    include=aapl[aapl.index.year==Year]
+    iinclude=aapl2[aapl2.index.year==Year]
+
+    include2=include[include.index.month==Month_index]
+    iinclude2=iinclude[iinclude.index.month==Month_index]
+
+    A=include2['4. close'].astype(float)
+    B=iinclude2['4. close'].astype(float)
+
+    A=pd.DataFrame(A)
+
+    jo=np.array(A['4. close'])
+
+    A['Date']=(A.index.day).astype(float)
+
+    jo_x=np.array(A['Date'])
 
 
-source = pd.DataFrame({
-  'Day': jo_x,
-  'Closing Value USD': jo
-})
+    B=pd.DataFrame(B)
+
+    B['Date']=(B.index.day).astype(float)
+
+
+    source = pd.DataFrame({
+      'Day': jo_x,
+      'Closing Value USD': jo
+    })
 
 
 
 
-st.write(alt.Chart(source).mark_line(point=True).encode(
-    alt.Y('Closing Value USD', scale=alt.Scale(zero=False)), x='Day'))
+    st.write(alt.Chart(source,title=f'{str(ticker[0])} daily closing values USD for {str(Month[0])} of {str(Year[0])}').mark_line(point=True).encode(
+        alt.Y('Closing Value USD', scale=alt.Scale(zero=False)), x='Day'))
+    
+except ValueError:
+    st.title("Enter the Stock , Year, and Month you want to be shown, then click post!")    
 
